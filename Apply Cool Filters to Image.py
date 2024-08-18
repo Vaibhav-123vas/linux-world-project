f60aa24ca5832dac8b58e30b79d9a3f403abb276
Vaibhav-123vas <126691958+Vaibhav-123vas@users.noreply.github.com>
2024-08-18 16:36:04 +0530

    Add files via upload

---
+++ Apply Cool Filters to Image.py
@@ -0,0 +1,5 @@
+def apply_cool_filters(image_path):
+    image = Image.open(image_path)
+    # Example filter: Add sunglasses (simplified example)
+    # For a more complex implementation, additional code is required
+    image.show()
